777adac6f55ebba398075c0cc7df54f3ee746220
Lomzyposh <alaminolomo@gmail.com>
2025-07-16 16:28:01 -0700

    save switch

--- src/SignUpPage/controller/signUpController.java
+++ src/SignUpPage/controller/signUpController.java
@@ -11,6 +11,8 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
@@ -20,6 +22,10 @@ import javafx.util.Duration;
 
 import java.io.IOException;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
 
 public class signUpController implements Initializable {
@@ -30,6 +36,13 @@ public class signUpController implements Initializable {
     public AnchorPane rootPane;
     @FXML
     public Button fadeTop;
+
+    @FXML
+    public TextField usernameField;
+    @FXML
+    public PasswordField passwordField;
+    @FXML
+    public PasswordField confirmPasswordField;
 
     public void goToLogin(ActionEvent event) throws IOException {
         Parent loginRoot = FXMLLoader.load(getClass().getResource("/LoginPage/view/login.fxml"));
@@ -70,4 +83,39 @@ public class signUpController implements Initializable {
             animation.play();
         });
     }
+
+
+    @FXML
+    public void submitForm() {
+
+//        Get Users Values
+        String userInput = usernameField.getText();
+        String userPassword = passwordField.getText();
+        String confirmPassword = confirmPasswordField.getText();
+
+//        Connect to the database
+        try {
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            String user = "sa2";
+            String pass = "00000000";
+            String url = "jdbc:sqlserver://localhost:1433;databaseName=To_DO_App;encrypt=true;trustServerCertificate=true";
+
+            Connection conn = DriverManager.getConnection(url, user, pass);
+
+            String query = "INSERT INTO users (username, password) VALUES (?,?)";
+
+            PreparedStatement preparedStatement = conn.prepareStatement(query);
+
+            preparedStatement.setString(1, userInput);
+            preparedStatement.setString(2, userPassword);
+
+
+            Boolean rs = preparedStatement.execute();
+            System.out.println("Success");
+
+        } catch (ClassNotFoundException | SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
 }
